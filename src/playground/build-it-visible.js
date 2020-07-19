9626b8ffbf0de08e5f1e6c6a2922c495884affbd
Yongyuan Ma <yongyuan.ma43@qmail.cuny.edu>
2020-07-19 14:17:13 -0400

    Indecision App Beta 1

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,26 +1,60 @@
-let visibility = false;
 
-const toggleVisibility = () => {
-  visibility = !visibility;
-  render();
-};
-
-const render = () => {
-  const jsx = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visibility ? 'Hide details' : 'Show details'}
-      </button>
-      {visibility && (
+class Visibility extends React.Component{
+  constructor(props){
+    super(props);
+    this.toggleVisibility=this.toggleVisibility.bind(this);
+    this.state={
+       visibility:false
+    }
+  }
+  toggleVisibility(){
+    this.setState((preState)=>{
+      return{
+      visibility:!preState.visibility
+      }
+    })
+  }
+  render(){
+    return(
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.toggleVisibility}>{this.state.visibility?'Hidden details':'Show Details'}</button>
+     
+      {this.state.visibility &&(
         <div>
-          <p>Hey. These are some details you can now see!</p>
+          <p>Hey,These are some details you can see!</p>
         </div>
       )}
-    </div>
-  );
+      </div>
+  )
+}
+}
 
-  ReactDOM.render(jsx, document.getElementById('app'));
-};
+ReactDOM.render(<Visibility/>,document.getElementById('app'))
 
-render();
+// let visibility = false;
+
+// const toggleVisibility = () => {
+//   visibility = !visibility;
+//   render();
+// };
+
+// const render = () => {
+//   const jsx = (
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <button onClick={toggleVisibility}>
+//         {visibility ? 'Hide details' : 'Show details'}
+//       </button>
+//       {visibility && (
+//         <div>
+//           <p>Hey. These are some details you can now see!</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+
+//   ReactDOM.render(jsx, document.getElementById('app'));
+// };
+
+// render();
